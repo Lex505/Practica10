54b528f6a6875e41d133c19e4fdac0a912b8cef8
chenaoh <cdhenao@gmail.com>
2024-09-18 21:14:52 -0600

    Se agrega el Proyecto

---
+++ src/main/java/com/practica/mavenproject2/Mavenproject2.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.practica.mavenproject2;
+
+/**
+ * Clae que realiza operaciones basicas
+ * @author AlexisCinerosAviles
+ */
+public class Mavenproject2 {
+    /*
+    *Suma dos numeros enteros.
+    *
+    *@param num1 Primer numero entero.
+    *@param num2 Segundo numero entero.
+    *@return La suma de los dos numros.
+    */
+    public int resta(int num1, int num2){
+        return num1-num2;
+    }
+}
+
